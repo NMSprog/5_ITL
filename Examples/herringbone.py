8c8f7b43e1a0c45b0ca33227c25ef36e4d5c091d
Oleg Kishinskiy <oleg.kishinskiy@gmail.com>
2020-06-28 20:17:30 +0300

    add example

--- Examples/herringbone.py
+++ Examples/herringbone.py
@@ -9,5 +9,25 @@
            ***********
 '''
 
+#Пример - 1
 for i in range(1, 20, 2):  #функция range() указывает начальное значение, конечное значение и шаг.
-    print('{:^20}'.format('*'*i)) # Печатаем символ выравнивая его по центру
+    print('{:^20}'.format('*' * i))  # Печатаем символ выравнивая его по центру
+
+
+
+#Пример -2
+SPACE = ' '
+STRAR = '*'
+
+rows = int(input('Укажите размер ёлочки: '))
+spaces = rows-1
+stars = 1
+
+for i in range(rows):
+    print(
+        (SPACE*spaces) +
+        (STRAR*stars) +
+        (SPACE*spaces)
+    )
+    stars += 2
+    spaces -= 1
